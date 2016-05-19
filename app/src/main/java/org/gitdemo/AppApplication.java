19f895e06e38f666ae4a9a8bad15cc535e54e504
Jakub Karbowiak <jakub.karbowiak@capgemini.com>
2016-05-19 14:26:40 +0200

    Another :) added

--- app/src/main/java/org/gitdemo/AppApplication.java
+++ app/src/main/java/org/gitdemo/AppApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- Example comment extended. :) :) :)
- :) Added
+ * Example comment extended. :) :) :)
+ * :) Added
+ * Example comment extended. :) :) Another :) added
  */
 @SpringBootApplication
 public class AppApplication {
