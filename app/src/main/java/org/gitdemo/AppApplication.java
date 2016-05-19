7bbcbfc9fd440265c16279cdcb4c4d93c1fd3927
Jakub Karbowiak <jakub.karbowiak@capgemini.com>
2016-05-19 13:54:31 +0200

    intellij files ignored, added JavaDoc

--- app/src/main/java/org/gitdemo/AppApplication.java
+++ app/src/main/java/org/gitdemo/AppApplication.java
@@ -3,6 +3,9 @@ package org.gitdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Example comment
+ */
 @SpringBootApplication
 public class AppApplication {
 
