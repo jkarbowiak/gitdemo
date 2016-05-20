bd2aa1c156ac27dc5c2ca91691dd3cdba698eb37
Jakub Karbowiak <jakub.karbowiak@capgemini.com>
2016-05-20 13:59:06 +0200

    Conflict commit 2

--- app/src/main/java/org/gitdemo/AppApplication.java
+++ app/src/main/java/org/gitdemo/AppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Conflict comment.
+ * Conflict comment. 2
  */
 @SpringBootApplication
 public class AppApplication {
