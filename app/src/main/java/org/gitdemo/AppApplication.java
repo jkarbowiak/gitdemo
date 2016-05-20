d586d801fa75960bfe25035cd0a269fe9fbde94a
Jakub Karbowiak <jakub.karbowiak@capgemini.com>
2016-05-20 14:16:51 +0200

    before rebase master

--- app/src/main/java/org/gitdemo/AppApplication.java
+++ app/src/main/java/org/gitdemo/AppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Before rebase
+ * Before rebase master
  */
 @SpringBootApplication
 public class AppApplication {
